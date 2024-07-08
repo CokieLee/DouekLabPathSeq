f19de7035c60750aea7316297b1c3b0dd826a1c5
Cokie Parker <Cokie@Parkertong.net>
2024-07-08 14:58:05 -0400

    reformatted but not yet running tests folder added

---
+++ Tests/runTestBowtieUnmasked.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+perl /hpcdata/vrc/vrc1_data/douek_lab/projects/PathSeq/cokie_pathseqtest0/PathSeq/Code/submit_for_given_samples.pl /hpcdata/vrc/vrc1_data/douek_lab/projects/PathSeq/cokie_pathseqtest0/PathSeq/Code /hpcdata/vrc/vrc1_data/douek_lab/projects/PathSeq/cokie_pathseqtest0/PathSeq/Tests/2023020_EVD68_NS_Test/InputData 2023020_EVD68_NS 0 0 RNA /hpcdata/vrc/vrc1_data/douek_lab/projects/PathSeq/cokie_pathseqtest0/PathSeq/Tests/2023020_EVD68_NS_Test/TestRun_GeneratedData/
