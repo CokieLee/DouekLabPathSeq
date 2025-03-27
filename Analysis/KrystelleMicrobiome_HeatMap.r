47bbfdbec7a2a34b7988e79d556037a32e4f394f
Cokie Parker <cokie@parkertong.net>
2025-03-26 21:15:33 -0400

    change pathseq heatmap script to output its modified data formats to file, so that they can be more easily used by other scripts

--- Analysis/KrystelleMicrobiome_HeatMap.r
+++ Analysis/KrystelleMicrobiome_HeatMap.r
@@ -2,13 +2,16 @@ library("dplyr")
 library("ggplot2")
 
 ## Define variable paths
-
 # import csv file with genus counts
-pathseqPath <- "/data/vrc_his/douek_lab/projects/PathSeq/Krystelle/pathseqResults/diversityMetrics/RNA_genus_tpm.csv"
+pathseqFilteredPath <- "/data/vrc_his/douek_lab/projects/PathSeq/Krystelle/pathseqResults/summaryFormatData/pathseq_genus_tpm_filtered.csv"
+pathseqUnfilteredPath <- "/data/vrc_his/douek_lab/projects/PathSeq/Krystelle/pathseqResults/pathseqTaxonomicSummaries/RNA_genus_tpm.csv"
 sampleSheetPath <- "/data/vrc_his/douek_lab/projects/PathSeq/Krystelle/Moritz_Sample_sheet_infection_status.txt"
 
 # output paths
-pathseqHighestGenusCountsPath <- "/data/vrc_his/douek_lab/projects/PathSeq/Krystelle/pathseqResults/diversityMetrics/HighestCountGenuses.csv"
+topGenusListPath <- "/data/vrc_his/douek_lab/projects/PathSeq/Krystelle/pathseqResults/summaryFormatData/Top10GenusList.csv"
+topGenusInfoPath <- "/data/vrc_his/douek_lab/projects/PathSeq/Krystelle/pathseqResults/summaryFormatData/Top10Genus_CountsAndTreatment_filtered.csv"
+topGenusInfoPathUnfiltered <- "/data/vrc_his/douek_lab/projects/PathSeq/Krystelle/pathseqResults/summaryFormatData/Top10Genus_CountsAndTreatment_unfiltered.csv"
+topN = 10
 
 ###############################################################################
 ## Define data
@@ -21,11 +24,10 @@ sampleSheet <-
            !str_detect(tolower(Sample.name), tolower("water")))
 
 # define pathseq all
-pathseqCounts <- read.csv(pathseqPath) %>%
+pathseqCounts <- read.csv(pathseqFilteredPath) %>%
   subset(genus != "taxonomy") %>%
   mutate(across(-1, as.numeric)) %>%
   replace(is.na(.), 0) %>%
-  rename_with(~ str_split(., "_", simplify = TRUE)[, 2], -1) %>%
   mutate(genus = (sapply(str_split(genus, "_"), function(x) paste0("g_", tail(x, 1)) ) ) ) %>%
   filter(genus != "g_")
 
@@ -35,6 +37,24 @@ pathseqTransposed <- pathseqTransposed[-1, ]
 
 pathseqMergeTreat <- pathseqTransposed %>%
   bind_cols(Sample.name = rownames(pathseqTransposed)) %>%
+  merge(sampleSheet, by = "Sample.name") %>%
+  mutate(across(-all_of(c("HIV.status", "Sample.name")), ~as.numeric(.)))
+
+# define pathseq all for unfiltered data
+pathseqUnfilteredCounts <- read.csv(pathseqUnfilteredPath) %>%
+  subset(genus != "taxonomy") %>%
+  mutate(across(-1, as.numeric)) %>%
+  replace(is.na(.), 0) %>%
+  rename_with(~str_split(., "_", simplify = TRUE)[, 2], -1) %>%
+  mutate(genus = (sapply(str_split(genus, "_"), function(x) paste0("g_", tail(x, 1)) ) ) ) %>%
+  filter(genus != "g_")
+
+pathseqUnfilteredTransposed <- t(pathseqUnfilteredCounts)
+colnames(pathseqUnfilteredTransposed) <- pathseqUnfilteredTransposed[1, ]
+pathseqUnfilteredTransposed <- pathseqUnfilteredTransposed[-1, ]
+
+pathseqUnfilteredMergeTreat <- pathseqUnfilteredTransposed %>%
+  bind_cols(Sample.name = rownames(pathseqUnfilteredTransposed)) %>%
   merge(sampleSheet, by = "Sample.name") %>%
   mutate(across(-all_of(c("HIV.status", "Sample.name")), ~as.numeric(.)))
 
@@ -50,9 +70,16 @@ pathseqSummedCounts <- pathseqTransposed %>%
   rename_with(~ c("counts")) %>%
   arrange(desc(counts))
 
-pathseqHighestGenuses <- head(pathseqSummedCounts, 30)
-write.csv(pathseqHighestGenuses, pathseqHighestGenusCountsPath, row.names = TRUE)
+pathseqHighestGenuses <- head(pathseqSummedCounts, topN)
+write.csv(pathseqHighestGenuses, topGenusListPath, row.names = TRUE)
 
+topCountsAndInfo <- pathseqMergeTreat %>%
+  select( "HIV.status", "Sample.name", any_of(rownames(pathseqHighestGenuses)) )
+write.csv(topCountsAndInfo, topGenusInfoPath, row.names = FALSE)
+
+topUnfilteredCountsAndInfo <- pathseqUnfilteredMergeTreat %>%
+  select( "HIV.status", "Sample.name", any_of(rownames(pathseqHighestGenuses)) )
+write.csv(topUnfilteredCountsAndInfo, topGenusInfoPathUnfiltered, row.names = FALSE)
 ###############################################################################
 ## define heatmap
 
