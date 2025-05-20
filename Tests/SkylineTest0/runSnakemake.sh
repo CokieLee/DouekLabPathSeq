af327526ad2381a3f9f000a7e2b8385a4ea0eb65
Cokie Parker <cokie@parkertong.net>
2025-05-20 11:40:21 -0400

    Changed testing folders

--- Tests/SkylineTest0/runSnakemake.sh
+++ Tests/SkylineTest0/runSnakemake.sh
@@ -5,8 +5,10 @@
 #SBATCH --mem=200G
 #SBATCH -t 4:00:00
 
-module load snakemake
+module load snakemake/7.22.0
 
-snakemake -p -c $SLURM_CPUS_PER_TASK --latency-wait 240
+snakemake -s ../../SlurmBaseCode/Snakefile \
+	  --profile /data/vrc_his/douek_lab/projects/PathSeq/Krystelle/skyline_profile/ \
+	  -p -c $SLURM_CPUS_PER_TASK --latency-wait 240
 
 echo "RAN SNAKEMAKE"
